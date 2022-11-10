755e6406dd28eac9794c5eb8699b1b57cdee8774
Vyktorr22 <117760730+Vyktorr22@users.noreply.github.com>
2022-11-10 10:56:06 -0600

    Update Hola_Mundo.java

--- Hola_Mundo.java
+++ Hola_Mundo.java
@@ -1,7 +1,7 @@
 
 public class Hola_Mundo {
 	public static void main (String args[]) {
-		System.out.println("HOLA MUNDO ]Aqui nada mas usando java xddd nnn");
+		System.out.println("Silvia, me gustas mucho <3.<3");
 		
 	}
 }
